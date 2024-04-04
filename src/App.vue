b4e2ee6858579d4283d87bafa0d76175b505d9c5
Inja-m <inja@mainhardts.de>
2024-04-04 09:37:59 +0200

    customise menubar preset + light/dark screen in taillwind.conf

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,58 @@
 <template>
   <Menubar :model="items">
     <template #item="{ item, props, hasSubmenu }">
-      <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
-        <a v-ripple :href="href" v-bind="props.action" @click="navigate">
-          <span :class="item.icon" />
-          <span class="ml-2">{{ item.label }}</span>
+        <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
+            <a v-ripple :href="href" v-bind="props.action" @click="navigate">
+                <span :class="item.icon" />
+                <span class="ml-2">{{ item.label }}</span>
+            </a>
+        </router-link>
+        <a v-else v-ripple :href="item.url" :target="item.target" v-bind="props.action">
+            <span :class="item.icon" />
+            <span class="ml-2">{{ item.label }}</span>
+            <span v-if="hasSubmenu" class="pi pi-fw pi-angle-down ml-2" />
         </a>
-      </router-link>
-      <a v-else v-ripple :href="item.url" :target="item.target" v-bind="props.action">
-        <span :class="item.icon" />
-        <span class="ml-2">{{ item.label }}</span>
-        <span v-if="hasSubmenu" class="pi pi-fw pi-angle-down ml-2" />
-      </a>
     </template>
   </Menubar>
   <RouterView />
 </template>
 
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView, useRoute } from 'vue-router'
 import Menubar from 'primevue/menubar'
 
-import { ref } from "vue"
+import { ref, watch } from 'vue'
 
+const route = useRoute()
 
 const items = ref([
   {
-    label: 'Home',
-    icon: 'pi pi-link',
-    route: '/about'
+    label: 'Über mich',
+    //icon: 'pi pi-user',
+    route: '/about',
+    active: route.path === '/about'
   },
   {
     label: 'Projekte',
-    icon: 'pi pi-link',
-    route: '/about'
+    //icon: 'pi pi-folder-open',
+    route: '/projects',
+    active: route.path === '/projects'
   },
   {
-    label: 'Über mich',
-    icon: 'pi pi-link',
-    route: '/about'
-  },
+    label: 'Home',
+    //icon: 'pi pi-home',
+    route: '/',
+    active: route.path === '/'
+  }
+ 
 ])
+
+watch(route, (to, from) => {
+  items.value.forEach(item => {
+    item.active = item.route === to.path;
+  })
+})
 </script>
 
-<style></style>
+<style>
+</style>
