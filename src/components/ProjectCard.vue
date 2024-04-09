aec5f5428b5c80bde7cf964eef2523874c1cb488
Inja-m <inja@mainhardts.de>
2024-04-09 16:20:18 +0200

    add Projectimages

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,11 +1,11 @@
 <template>
-	<div class="w-full sm:max-w-full sm:flex my-12" :class="`bg-${bgColour}-200`">
-      <div class="h-48 sm:h-48 sm:w-48 flex-none bg-cover rounded-t sm:rounded-t-none sm:rounded-l text-center overflow-hidden">
-        <img class="h-48 sm:h-48 sm:w-48 flex-none bg-cover rounded-t sm:rounded-t-none sm:rounded-l text-center overflow-hidden" :src = "imageSrc" alt="Logo" height = "48" width = "48"/>
+	<div class="w-full sm:max-w-full sm:flex my-12  space-x-12" :class="`bg-${bgColour}-200`">
+      <div class="sm:h-64 h-48 flex-none justify-center p-4" :class="imgLeft ? 'order-2' : 'order-1'">
+        <img class="sm:h-64 h-48 flex-none item-center" :src = "imageSrc" alt="Projektbild" />
       </div>
-      <div class="rounded-b sm:rounded-b-none sm:rounded-r p-4 flex flex-col justify-center leading-normal">
-        <div class="text-gray-900 font-bold text-xl mb-2">{{ title }}</div>
-        <p class="text-gray-700 text-base">{{ content }}</p>
+      <div class="flex flex-col justify-center leading-normal"  :class="imgLeft ? 'order-1' : 'order-2'">
+        <div class="text-gray-900 font-bold text-xl mb-2 mr-12">{{ title }}</div>
+        <p class="text-gray-700 text-base text-justify mr-12">{{ content }}</p>
       </div>
   </div>
 </template>
@@ -16,7 +16,8 @@ const props = defineProps({
   title: String,
   content: String,
   imageSrc: String,
-  bgColour: String
+  bgColour: String,
+  imgLeft: Boolean
 })
 
 </script>
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,15 +1,17 @@
 <template>
-	<h1>Projekte</h1>
-	<div v-for="project in projectContent" :key="project.id">
-    <ProjectCard :title="project.title" :content="project.description" :imageSrc="project.imageSrc" :bgColour="project.colour"></ProjectCard>
-    </div>
-	
+  <div v-for="project in projectContent" :key="project.id">
+    <ProjectCard
+      :title="project.title"
+      :content="project.description"
+      :imageSrc="project.imageSrc"
+      :bgColour="project.colour"
+	  :imgLeft="project.id === 2 ? true : false"
+    ></ProjectCard>
+  </div>
 </template>
-  
-<script setup>
 
+<script setup>
 import ProjectCard from '@/components/ProjectCard.vue'
 
 import projectContent from '@/content/projects.json'
-
 </script>
