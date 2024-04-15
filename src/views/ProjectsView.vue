d9bd5e2c85da14e538aeb41a856cfec12becfc48
Inja-m <inja@mainhardts.de>
2024-04-15 19:00:18 +0200

    remove duplicate container

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,20 +1,16 @@
 <template>
 	<div class="container">
 		<h1 class="text-center sm:text-left">
-      Entdecke meine Projekte
+			Entdecke meine Projekte
 		</h1>
 		<h2 class="text-center sm:text-end py-2">
 			Jedes Projekt ist eine Gelegenheit für mich, meinen Horizont zu erweitern und Neues zu lernen.
 		</h2>
-		
-		<div v-for="project in projectContent" :key="project.id" class="container mt-10 sm:mt-16">
-				<ProjectCard :id="project.id" 
-					:title="project.title" 
-					:content="project.description" 
-					:imageSrc="project.imageSrc"
-					:bgColour="project.colour" 
-					:imgLeft="project.id === 2 ? true : false"></ProjectCard>
-		</div>
+	</div>
+
+	<div v-for="project in projectContent" :key="project.id" class="container mt-10 sm:mt-16">
+		<ProjectCard :id="project.id" :title="project.title" :content="project.description" :imageSrc="project.imageSrc"
+			:bgColour="project.colour" :imgLeft="project.id === 2 ? true : false"></ProjectCard>
 	</div>
 </template>
 
